7239a49114fa8874289e0b8b83318aaaab1bb096
Loay Ghreeb <loayahmed655@gmail.com>
2023-12-16 14:46:45 +0200

    api call to download all quran pages

--- src/Main.java
+++ src/Main.java
@@ -41,23 +41,47 @@ public class Main extends Application {
     }
 }
 
-
 /*
 public class Main {
     public static void main(String[] args) {
-        List <Page> pages = new ArrayList<>();
         try {
-            for (int i = 1; i < 605; i++) {
+            for (int i = 605; i <= 605; i++) {
+                URL url = new URL("https://api.quran.com/api/v4/verses/by_page/" + i + "?words=true&word_fields=location,text_uthmani,text_imlaei,verse_key,code_v1,code_v2&fields=text_uthmani_simple,text_imlaei,chapter_id,code_v1,code_v2");
+                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+                connection.setRequestMethod("GET");
+                connection.connect();
+                int responseCode = connection.getResponseCode();
+                if (responseCode == HttpURLConnection.HTTP_OK) {
+                    BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                    StringBuilder response = new StringBuilder();
+                    String line;
+                    while ((line = reader.readLine()) != null) {
+                        response.append(line);
+                    }
+                    reader.close();
+                    ObjectMapper objectMapper = new ObjectMapper();
+                    JsonNode jsonNode = objectMapper.readTree(response.toString());
+
+                    // Write JSON to a file
+                    String fileName = i + ".json";
+                    try (BufferedWriter writer = new BufferedWriter(new FileWriter("assets/quran" + File.separator + fileName))) {
+                        writer.write(jsonNode.toPrettyString());
+                        System.out.println("JSON data saved to: " + "assets/quran" + File.separator + fileName);
+                    }
+                }
+
                 File jsonFile = new File("assets/quran/" + i + ".json");
                 ObjectMapper objectMapper = new ObjectMapper();
-                Page page = objectMapper.readValue(jsonFile, Page.class);
-                pages.add(page);
+                Juz juz = objectMapper.readValue(jsonFile, Juz.class);
+                for (Verse verse : juz.getVerses()) {
+                    System.out.println("Verse Key: " + verse.getVerse_key());
+//                List<Word> words = verse.getWords();
+//                for(Word word : words)
+//                    System.out.println("Word Text Uthmani: " + word.getLocation());
+                }
             }
         } catch (Exception e) {
             e.printStackTrace();
-        }
-        for(Page page : pages){
-            System.out.println(page.getVerses().getFirst().getPage_number());
         }
     }
 }
