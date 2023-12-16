89fb99543cea4cbb8d1f3a08ab194bdc61781919
Loay Ghreeb <loayahmed655@gmail.com>
2023-12-16 11:15:55 +0200

    added project files

---
+++ src/Controller.java
@@ -0,0 +1,8 @@
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.text.TextFlow;
+
+public class Controller {
+    @FXML
+    TextFlow textFlow1;
+}
---
+++ src/Main.java
@@ -0,0 +1,28 @@
+import com.fasterxml.jackson.databind.ObjectMapper;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.control.SplitPane;
+import javafx.scene.text.*;
+
+import java.io.*;
+
+
+public class Main extends Application {
+    @Override
+    public void start(Stage stage) throws IOException {
+//        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("sample.fxml"));
+//        Scene scene = new Scene(fxmlLoader.load(), 460, 950);
+        SplitPane sp = new SplitPane();
+        ObjectMapper objectMapper = new ObjectMapper();
+        Font f = Font.loadFont(new FileInputStream(new File("assets/fonts/QCFV1/QCF_P003.ttf")), 33);
+        Text text = new Text("ﭑ ﭒ ﭓ ﭔ ﭕ ﭖ ﭗ ﭘ ﭙ ﭚ ﭛ ﭜ ﭝ ﭞ ﭟ ﭠ ﭡ ﭢ ﭣ ﭤ ﭥ ﭦ ﭧ ﭨ ﭩ ﭪ ﭫ ﭬ ﭭ ﭮ ﭯ ﭰ ﭱ ﭲ ﭳ ﭴ ﭵ ﭶ ﭷ ﭸ ﭹ ﭺ ﭻ ﭼ ﭽ ﭾ ﭿ ﮀ ﮁ ﮂ ﮃ ﮄ ﮅ ﮆ ﮇ ﮈ ﮉ ﮊ ﮋ ﮌ ﮍ ﮎ ﮏ ﮐ ﮑ ﮒ ﮓ ﮔ ﮕ ﮖ ﮗ ﮘ ﮙ ﮚ ﮛ ﮜ ﮝ ﮞ ﮟ ﮠ ﮡ ﮢ ﮣ ﮤ ﮥ ﮦ ﮧ ﮨ ﮩ ﮪ ﮫ ﮬ ﮭ ﮮ ﮯ ﮰ ﮱ ﯓ ﯔ ﯕ ﯖ ﯗ ﯘ ﯙ ﯚ ﯛ ﯜ ﯝ ﯞ ﯟ ﯠ ﯡ ﯢ ﯣ ﯤ ﯥ ﯦ ﯧ ﯨ ﯩ ﯪ ﯫ ﯬ ﯭ ﯮ ﯯ ﯰ ﯱ ﯲ ﯳ ﯴ ﯵ ﯶ ﯷ ﯸ ﯹ ﯺ ﯻ ﯼ ﯽ ﯾ ﯿ");
+        text.setFont(f);
+        TextFlow textFlow = new TextFlow(text);
+        textFlow.setTextAlignment(TextAlignment.CENTER);
+        Scene scene = new Scene(textFlow, 460, 950);
+        stage.setScene(scene);
+        stage.show();
+    }
+}
