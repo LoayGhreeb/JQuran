4ced36a4b8c63b691a96b64620b2875c6aa7d30f
Loay Ghreeb <loayahmed655@gmail.com>
2023-12-26 15:14:01 +0200

    fix bug

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,9 +3,7 @@ module org.jquran.jquran {
     requires javafx.fxml;
     requires com.fasterxml.jackson.databind;
     requires org.controlsfx.controls;
-    requires spark.core;
     requires atlantafx.base;
-//    requires com.sparkjava.spark;
 
     opens org.jquran.jquran to javafx.fxml;
     exports org.jquran.jquran;
